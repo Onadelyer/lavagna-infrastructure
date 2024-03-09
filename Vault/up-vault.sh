75aee0025fcd73d5e5a15cdd3e9ba66fd6e6e681
Onadelyer <tokarmacs63@gmail.com>
2024-03-09 21:23:39 +0200

    1

--- Vault/up-vault.sh
+++ Vault/up-vault.sh
@@ -1,3 +1,7 @@
-minikube kubectl -- apply -f vault.namespace.yaml
-minikube kubectl -- apply -f vault.service.yaml
-minikube kubectl -- apply -f vault.deployment.yaml
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+helm repo add hashicorp https://helm.releases.hashicorp.com
+
+helm repo update
+
+helm install vault hashicorp/vault --namespace vault --create-namespace
--- start-cluster.sh
+++ start-cluster.sh
@@ -1,5 +1,7 @@
+#up manifests
 sh Jenkins/up-jenkins.sh
 sh Vault/up-vault.sh
+
 minikube addons enable registry
 minikube addons enable ingress
 minikube addons enable metrics-server
