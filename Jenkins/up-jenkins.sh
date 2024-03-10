28dabb1cc546b65c8c573315e665ddd30db91068
Onadelyer <tokarmacs63@gmail.com>
2024-03-10 18:32:00 +0200

    1

--- Jenkins/up-jenkins.sh
+++ Jenkins/up-jenkins.sh
@@ -1,5 +1,6 @@
 minikube kubectl -- apply -f jenkins.namespace.yaml
 minikube kubectl -- apply -f jenkins.serviceaccount.yaml
+minikube kubectl -- apply -f jenkins.volume.yaml
 minikube kubectl -- apply -f jenkins.token.yaml
 minikube kubectl -- apply -f jenkins.service.yaml
 minikube kubectl -- apply -f jenkins.deployment.yaml
